ef4802f2459be7330b052bb43fa3fe6db61cec1f
Logan Rakai <lmrakai@gmail.com>
2018-11-19 12:00:40 -0700

    S3 bucket check

---
+++ aws/s3/bucket_created.py
@@ -0,0 +1,14 @@
+import boto3
+import botocore
+
+# Lambda function
+def handler(event, context):
+    s3 = create_new_session(event).resource('s3')
+    return any(s3.buckets.iterator())
+
+# helpers
+def create_new_session(event):
+    return boto3.Session(
+        aws_access_key_id=event['credentials']['credential_id'],
+        aws_secret_access_key=event['credentials']['credential_key'],
+    )
