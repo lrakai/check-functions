be979f8d56cf75aba47e804e9f28b35e902a68c9
Logan Rakai <lmrakai@gmail.com>
2018-11-18 20:59:50 -0700

    User used password

---
+++ aws/users/used_password.py
@@ -0,0 +1,18 @@
+import boto3
+import botocore
+
+# lambda function
+def handler(event, context):
+	# init session
+    client = create_new_session(event).client('iam')
+    
+    payload = client.get_user(UserName='student')
+    
+    return True if 'PasswordLastUsed' in payload['User'] else False
+
+# helpers
+def create_new_session(event):
+    return boto3.Session(
+        aws_access_key_id=event['credentials']['credential_id'],
+        aws_secret_access_key=event['credentials']['credential_key'],
+    )
