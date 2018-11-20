e34a6bd600fe293b273d83b351d8379fa8d118a7
Logan Rakai <lmrakai@gmail.com>
2018-11-20 11:04:38 -0700

    Set session region

--- aws/s3/bucket_created.py
+++ aws/s3/bucket_created.py
@@ -11,4 +11,5 @@ def create_new_session(event):
     return boto3.Session(
         aws_access_key_id=event['credentials']['credential_id'],
         aws_secret_access_key=event['credentials']['credential_key'],
+      	region_name=event['region_id']
     )
